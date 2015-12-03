7576f47cf49a45a93642736bd421b4c26cbb48c9
Alek Williams <alekw@berkeley.edu>
2015-12-02 16:11:55 -0800

    used ZOH to discretize the dynamics

--- InvertedPendulum/systemDynamics.m
+++ InvertedPendulum/systemDynamics.m
@@ -9,7 +9,7 @@ g = 9.8;       % acceleration of gravity [m/s^2]
 l = 0.3;       % length of pendulum
 T = 0.02;      % discretization time
 
-% Discrete time dynamics
+% Continuous time dynamics
 d = I*(M+m) + M*m*l^2;
 c1 = -(I+m*l^2)*b/d;
 c2 = ((m^2)*g*l^2)/d;
@@ -18,10 +18,21 @@ c4 = m*g*l*(M+m)/d;
 c5 = (I+m*l^2)/d;
 c6 = m*l/d;
 
-A = [1 T 0 0;
-    0 1+c1*T c2*T 0;
-    0 0 1 T;
-    0 c3*T c4*T 1];
+Ac = [0 1 0 0;
+      0 c1 c2 0;
+      0 0 0 1;
+      0 c3 c4 0];
+Bc = [0 c5 0 c6]';
+Cc = eye(4);
+Dc = zeros(4,1);
 
-B = [0 c5*T 0 c6*T]';
+ctSys = ss(Ac,Bc,Cc,Dc);
+
+% Discrete time dynamics
+sys = c2d(ctSys, T, 'zoh');
+A = sys.A;
+B = sys.B;
 Bw = B;
+
+%%
+% save('sysDyn.mat', 'A', 'B', 'Bw')
